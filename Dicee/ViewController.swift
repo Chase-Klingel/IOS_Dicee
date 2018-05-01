114073e43eb70fa0f186155b208dbacbc689a15b
Chase-Klingel <klingelchase@gmail.com>
2018-05-01 16:58:31 -0700

    removing semicolons & moving core code into a reusable function

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var randomDiceIndex1 : Int = 0;
-    var randomDiceIndex2 : Int = 0;
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
     
     let diceArray = ["dice1", "dice2", "dice3",
                      "dice4", "dice5", "dice6"
-                    ];
+                    ]
     
     // @IBOutlet : changes the appearance of a ui element
     @IBOutlet weak var diceImageView1 : UIImageView!
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,10 +32,15 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        randomDiceIndex1     = Int(arc4random_uniform(6));
-        randomDiceIndex2     = Int(arc4random_uniform(6));
-        diceImageView1.image = UIImage(named : diceArray[randomDiceIndex1]);
-        diceImageView2.image = UIImage(named : diceArray[randomDiceIndex2]);
+       updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        randomDiceIndex1     = Int(arc4random_uniform(6))
+        randomDiceIndex2     = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named : diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named : diceArray[randomDiceIndex2])
     }
 
 }
